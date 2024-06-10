8b5d72a2a79f93c715d1f48fd0c5a831f9197d0f
lostslvtt <169078749+lostsdev@users.noreply.github.com>
2024-06-10 15:19:11 +0500

    create a delay to send commands when you're in air

---
+++ src/main/java/org/lostslvtt/airdelayplugin/AirDelayPlugin.java
@@ -0,0 +1,16 @@
+package org.lostslvtt.airdelayplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AirDelayPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
